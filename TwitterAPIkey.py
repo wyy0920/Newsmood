316faddb5fc2cda368bf6be75aef9065dd9d1bcb
wyy0920 <wyy0920@gmail.com>
2018-03-22 16:40:35 -0700

    added output

---
+++ TwitterAPIkey.py
@@ -0,0 +1,6 @@
+
+# Twitter API Keys
+consumer_key = "5Bcwba0iyoJKPVwPXoOtOySX2"
+consumer_secret = "IKs1k236IIwYer3lkmegGiPLEzndsOdKx4y5M8VcndbjwpjhNh"
+access_token = "836955714-nAYPHeLrLFFA8diVdoRk31FsKgMug8Cb1paHeMwJ"
+access_token_secret = "k3Ctzff4NYj9i8yls1IKwW98QEpAin0LZQFKTGJMNMj31"
